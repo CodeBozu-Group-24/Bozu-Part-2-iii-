47d644b60c2f5885fd167239bd721b5ecc527982
Prithwis Das <prithwisdas2017@gmail.com>
2022-01-05 07:51:06 +0530

    update

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ soup = BeautifulSoup(data, 'lxml')
 list1 = soup.findAll('h4')
 
 
-
 table = soup.find("table", {"class":"wikitable sortable"})
 rows = table.find_all("tr")
 rows = rows[1:]
@@ -28,6 +27,21 @@ NewsSources.insert(0, 'Politician')
 with open('news.csv', 'w') as f:
     writer_object = writer(f)
     writer_object.writerow(NewsSources)
-    #for i in range(len(title_text)):
-        #writer_object.writerow([title_text[i], writings[i], positivity[i]])
     f.close()   
+
+politicians = []
+with open('details.csv', 'r') as f:
+    reader = csv.reader(f)
+    amr_csv = list(reader)
+    for row in amr_csv[1:]:
+        politicians.append(row[0])
+    f.close()
+
+#the main list of politicians without repition of names
+politicians_reduced = []
+for ele in politicians:
+    if ele not in politicians_reduced:
+        politicians_reduced.append(ele)
+
+#iterate through each politician and search about them in each news channel
+

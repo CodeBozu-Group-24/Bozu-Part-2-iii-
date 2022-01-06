e967d43aab12e14b361cadc1236b8eee3d4f0ae1
Prithwis Das <prithwisdas2017@gmail.com>
2022-01-06 08:08:20 +0530

    update

--- main.py
+++ main.py
@@ -47,34 +47,111 @@ for ele in politicians:
 '''
 politicians = ['Donald Trump', 'Joe Biden', 'Barack Obama']
 scores = []
-#Donald Trump
-data = requests.get('https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump').text
-soup = BeautifulSoup(data, 'lxml')
-titles = soup.findAll('a', class_='qa-heading-link lx-stream-post__header-link')
 
+#BBC for Donald Trump
+data1 = requests.get('https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump').text
+soup1 = BeautifulSoup(data1, 'lxml')
+titles1 = soup1.findAll('a', class_='qa-heading-link lx-stream-post__header-link')
 
-
-title_text = []
-for title in titles:
-    title_text.append(title.get_text())
+title_text1 = []
+for title in titles1:
+    title_text1.append(title.get_text())
 
 #links = []
-writings = []
-for title in titles:
+writings1 = []
+for title in titles1:
     link = "https://www.bbc.com"+title["href"]
     #data_new = requests.get(link).text
     #soup_new = BeautifulSoup(data_new, 'lxml')
-    writingFinal = getTextFromArticle(link).encode("ascii", "ignore").decode()
-    writingFinal = re.sub("\n", " ", writingFinal)
-    writings.append(writingFinal)
+    writingFinal1 = getTextFromArticle(link).encode("ascii", "ignore").decode()
+    writingFinal1 = re.sub("\n", " ", writingFinal1)
+    writings1.append(writingFinal1)
 
-positivity = []    
-for writing in writings:
-    sid_obj = SentimentIntensityAnalyzer()
-    sentiment_dict = sid_obj.polarity_scores(writing)
-    positivity_score = sentiment_dict['pos']*100
-    positivity.append(positivity_score)
+positivity1 = []    
+for writing in writings1:
+    sid_obj1 = SentimentIntensityAnalyzer()
+    sentiment_dict1 = sid_obj1.polarity_scores(writing)
+    positivity_score1 = sentiment_dict1['pos']*100
+    positivity1.append(positivity_score1)
 
-scores.append(str(sum(positivity)/len(positivity)))
-#iterate through each politician and search about them in each news channel
+scores.append(str(sum(positivity1)/len(positivity1)))
 
+#BBC for Joe Biden
+data2 = requests.get('https://www.bbc.co.uk/search?q=Joe+Biden').text
+soup2 = BeautifulSoup(data2, 'lxml')
+titles2 = soup2.findAll('a', href=True, class_='ssrcss-atl1po-PromoLink e1f5wbog0')
+
+title_text2 = []
+for title in titles2:
+    title_text2.append(title.get_text())
+
+links = []    
+subs = 'href'
+res = [str(i) for i in titles2 if subs in str(i)]
+for ele in res:
+    link = ele.split(' ')[3]
+    i = link.index('"')
+    f = link.index('>')
+    links.append(link[(i+1):(f-1)])
+
+writings2 = []
+for link in links:
+    #data_new = requests.get(link).text
+    #soup_new = BeautifulSoup(data_new, 'lxml')
+    try:
+        writingFinal2 = getTextFromArticle(link).encode("ascii", "ignore").decode()
+        writingFinal2 = re.sub("\n", " ", writingFinal2)
+        writings2.append(writingFinal2)
+    except AttributeError:
+        writings2.append('No text existing currenly.')
+
+positivity2 = []    
+for writing in writings2:
+    sid_obj2 = SentimentIntensityAnalyzer()
+    sentiment_dict2 = sid_obj2.polarity_scores(writing)
+    positivity_score2 = sentiment_dict2['pos']*100
+    positivity2.append(positivity_score2)
+
+scores.append(str(sum(positivity2)/len(positivity2)))
+
+
+#BBC News for Barack Obama
+data3 = requests.get('https://www.bbc.co.uk/search?q=Barack+Obama').text
+soup3 = BeautifulSoup(data3, 'lxml')
+titles3 = soup3.findAll('a', href=True, class_='ssrcss-atl1po-PromoLink e1f5wbog0')
+
+title_text3 = []
+for title in titles3:
+    title_text3.append(title.get_text())
+
+links3 = []    
+subs = 'href'
+res = [str(i) for i in titles3 if subs in str(i)]
+for ele in res:
+    link = ele.split(' ')[3]
+    i = link.index('"')
+    f = link.index('>')
+    links3.append(link[(i+1):(f-1)])
+
+writings3 = []
+for link in links3:
+    #data_new = requests.get(link).text
+    #soup_new = BeautifulSoup(data_new, 'lxml')
+    try:
+        writingFinal3 = getTextFromArticle(link).encode("ascii", "ignore").decode()
+        writingFinal3 = re.sub("\n", " ", writingFinal3)
+        writings3.append(writingFinal3)
+    except AttributeError:
+        writings3.append('No text existing currenly.')
+print(writings3)
+positivity3 = []    
+for writing in writings3:
+    sid_obj3 = SentimentIntensityAnalyzer()
+    sentiment_dict3 = sid_obj3.polarity_scores(writing)
+    positivity_score3 = sentiment_dict3['pos']*100
+    positivity3.append(positivity_score3)
+
+print(positivity3)
+scores.append(str(sum(positivity3)/len(positivity3)))
+
+print(scores)
